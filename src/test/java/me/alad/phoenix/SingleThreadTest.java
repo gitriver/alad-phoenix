6ab11b83a1b609f7536b0373310e28b03d2c5a1b
gitriver <jasen04@163.com>
2016-05-16 11:46:59 +0800

    修改log4j配置

--- src/test/java/me/alad/phoenix/SingleThreadTest.java
+++ src/test/java/me/alad/phoenix/SingleThreadTest.java
@@ -1,5 +1,6 @@
 package me.alad.phoenix;
 
+import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
@@ -7,37 +8,88 @@ import java.util.Map;
 import me.alad.phoenix.pool.ConnectionManager;
 
 import org.apache.phoenix.jdbc.PhoenixConnection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 
 /**
  * thrift client test
  */
 public class SingleThreadTest {
-    public static void main(String[] args) throws SQLException {
-        final ApplicationContext context =
-                new ClassPathXmlApplicationContext("applicationContext-client.xml");
+    public static String baseDir = "G:/";
+    public static Logger LOG = LoggerFactory.getLogger(SingleThreadTest.class);
 
+
+    public static void main(String[] args) {
+        if (args.length > 0)
+            baseDir = args[0];
+        LOG.info("程序运行开始");
+        Map<String, String> map = Maps.newHashMap();
+        // map.put("cls.selfcate",
+        // "select count from cls.selfcate where taskid in(6,481) and busidate=20160314");
+        // map.put("cls.result",
+        // "select * from cls.result where taskid in(6,481) and busidate=20160314");
+        // map.put("cls.kwreltext",
+        // "select * from cls.kwreltext where taskid in(6,481) and busidate=20160314");
+        map.put("cls.textdetail",
+            "select * from cls.textdetail where taskid in(6,481) and busidate=20160314");
+        // map.put("tag.hottags",
+        // "select * from tag.hottags where taskid in(438,480) and busidate=20160314");
+        // map.put("tag.texttag",
+        // "select * from tag.texttag where taskid in(438,480) and busidate=20160314");
+        // map.put("tag.usertag",
+        // "select * from tag.usertag where taskid in(438,480) and busidate=20160314");
+
+        final ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext-phoenix.xml");
         ConnectionManager connManager = (ConnectionManager) context.getBean("connectionManagerBean");
-        PhoenixConnection conn = connManager.getConn();
-        List<Map<String, Object>> list2 =
-                PhoenixUtil.executeQuery(conn, "select * from wifi.portallog  limit 10");
-        System.out.println(list2);
-        
-        String sql =
-                "upsert into log.action(createat,sn,country)values(?,next value for logsn,?)";
-        List<List> datas = Lists.newArrayList();
-        for(int i=0;i<10;i++){
-            List row = Lists.newArrayList();
-            row.add(i+1);
-            row.add("中国");
-            datas.add(row);
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String tableName = entry.getKey();
+            LOG.info("开始处理{}表", tableName);
+            exportData(entry.getKey(), entry.getValue(), connManager);
+            LOG.info("表{}处理完成", tableName);
         }
-        PhoenixUtil.updateBatchPreparedStatement(conn, sql, datas);
-        
-        connManager.closeConn(conn);
+    }
+
+
+    public static void exportData(String tableName, String sql, ConnectionManager connManager) {
+        try {
+
+            PhoenixConnection conn = connManager.getConn();
+            List<Map<String, Object>> list = PhoenixUtil.executeQuery(conn, sql);
+
+            PrintWriter out = new PrintWriter(baseDir + tableName + ".txt");
+
+            StringBuffer sb = new StringBuffer();
+            char sep = 239;
+            int num = 0;
+            for (Map<String, Object> row : list) {
+                for (Map.Entry<String, Object> entry : row.entrySet()) {
+                    if (entry.getValue() != null) {
+                        sb.append(entry.getValue());
+                    }
+                    sb.append(sep);
+                }
+                out.println(sb.substring(0, sb.length() - 1));
+                sb.setLength(0);
+                num++;
+                if (num > 1000) {
+                    LOG.info("表{}已经处理{}条", tableName, num);
+                    out.flush();
+                    num = 0;
+                }
+            }
+            out.close();
+            LOG.info("表{}处理了{}条", tableName, num);
+            connManager.closeConn(conn);
+        }
+        catch (Exception e) {
+            LOG.error(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
